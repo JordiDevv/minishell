1a89905cc6c91c05e068abe07c04a149cc185838
rhernand <rhernand@student.42malaga.com>
2024-11-28 21:59:55 +0100

    add libft submodule and set working folders

---
+++ inc/parser.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 21:32:22 by rhernand          #+#    #+#             */
+/*   Updated: 2024/11/28 21:58:03 by rhernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+# include "libft/inc/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+
+#endif
