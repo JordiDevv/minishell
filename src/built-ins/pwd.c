f86c2e72efa8c7b9f137bac71a4cefdff42389e4
JordiDevv <jordidevv@gmail.com>
2024-12-18 13:17:14 +0100

    Se ha actualizado la función ex_pwd para que reciba el envp y utilice ft_find_var para encontrar la variable PWD, para posteriormente la imprime.

--- src/built-ins/pwd.c
+++ src/built-ins/pwd.c
@@ -6,17 +6,17 @@
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 00:24:54 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2024/12/18 13:00:06 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2024/12/18 13:13:07 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/executor.h"
 
-void	ex_pwd(char **env)
+void	ex_pwd(char **envp)
 {
-	char	pwd;
+	char	pwd_value;
 
-	pwd = ft_find_var()
+	pwd_value = ft_find_var(envp, "PWD");
 	if (!pwd_value)
 	{
 		printf(Y "Error: pwd is corrupted" RE);
