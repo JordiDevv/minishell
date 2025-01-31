9bf0534f7e84227c41be356002a3a3787f59e92f
JordiDevv <jordidevv@gmail.com>
2025-01-31 18:20:53 +0100

    Se ha unificado al main para dejarlo más clean de cara a una versión funcional.

--- src/main.c
+++ src/main.c
@@ -1,130 +1,141 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_testing.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 13:38:57 by rhernand          #+#    #+#             */
-/*   Updated: 2025/01/31 18:17:41 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2025/01/31 18:19:24 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../libft/libft.h"
-// #include "../inc/parser.h"
+#include "../inc/parser.h"
+#include "../inc/executor.h"
 
-// void	ft_print_list(t_msh *msh)
-// {
-// 	t_list	*aux;
-// 	int		i;
-// 	int		j;
+void	ft_print_list(t_msh *msh)
+{
+	t_list	*aux;
+	int		i;
+	int		j;
 
-// 	i = 0;
-// 	j = 1;
-// 	aux = msh->lst;
-// 	while (aux)
-// 	{
-// 		printf("\ncommand number %d\n", j);
-// 		printf("cmd built =  %s\n", ((t_cmd *) aux->content)->built);
-// 		printf("cmd full =  %s\n", ((t_cmd *) aux->content)->full);
-// 		printf("cmd input =  %s\n", ((t_cmd *) aux->content)->input);
-// 		printf("cmd del =  %s\n", ((t_cmd *) aux->content)->del);
-// 		printf("cmd output =  %s\n", ((t_cmd *) aux->content)->output);
-// 		printf("cmd append =  %s\n", ((t_cmd *) aux->content)->append);
-// 		while (((t_cmd *)aux->content)->split \
-// 			&& ((t_cmd *)aux->content)->split[i])
-// 		{
-// 			printf("split %d = %s\n", i, ((t_cmd *) aux->content)->split[i]);
-// 			i++;
-// 		}
-// 		i = 0;
-// 		j++;
-// 		aux = aux->next;
-// 	}
-// }
+	i = 0;
+	j = 1;
+	aux = msh->lst;
+	while (aux)
+	{
+		printf("\ncommand number %d\n", j);
+		printf("cmd built =  %s\n", ((t_cmd *) aux->content)->built);
+		printf("cmd full =  %s\n", ((t_cmd *) aux->content)->full);
+		printf("cmd input =  %s\n", ((t_cmd *) aux->content)->input);
+		printf("cmd del =  %s\n", ((t_cmd *) aux->content)->del);
+		printf("cmd output =  %s\n", ((t_cmd *) aux->content)->output);
+		printf("cmd append =  %s\n", ((t_cmd *) aux->content)->append);
+		while (((t_cmd *)aux->content)->split \
+			&& ((t_cmd *)aux->content)->split[i])
+		{
+			printf("split %d = %s\n", i, ((t_cmd *) aux->content)->split[i]);
+			i++;
+		}
+		i = 0;
+		j++;
+		aux = aux->next;
+	}
+}
 
-// /*Just an ASCII artpiece to know we entered MINISHELL*/
+/*Just an ASCII artpiece to know we entered MINISHELL*/
 
-// void	ft_draw(void)
-// {
-// 	const char	*ascii = \
-// 	"         _nnnn_\n" \
-// 	"        dGGGGMMb     ,\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\".\n" \
-// 	"       @p~qp~~qMb    |     MINISHELL     |\n" \
-// 	"       M|@||@) M|    | jsanz-bo rhernand |\n" \
-// 	"       @,----.JM| -'_;...................'\n" \
-// 	"      JS^\\__/  qKL\n" \
-// 	"     dZP        qKRb\n" \
-// 	"    dZP          qKKb\n" \
-// 	"   fZP            SMMb\n" \
-// 	"   HZM            MMMM\n" \
-// 	"   FqM            MMMM\n" \
-// 	" __| \".        |\\dS\"qML\n" \
-// 	" |    `.       | `' \\Zq\n" \
-// 	"_)      \\.___.|     .'\n" \
-// 	"\\____   )MMMMMM|   .'\n" \
-// 	"     `-'       `--'\n";
+void	ft_draw(void)
+{
+	const char	*ascii = \
+	"         _nnnn_\n" \
+	"        dGGGGMMb     ,\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\".\n" \
+	"       @p~qp~~qMb    |     MINISHELL     |\n" \
+	"       M|@||@) M|    | jsanz-bo rhernand |\n" \
+	"       @,----.JM| -'_;...................'\n" \
+	"      JS^\\__/  qKL\n" \
+	"     dZP        qKRb\n" \
+	"    dZP          qKKb\n" \
+	"   fZP            SMMb\n" \
+	"   HZM            MMMM\n" \
+	"   FqM            MMMM\n" \
+	" __| \".        |\\dS\"qML\n" \
+	" |    `.       | `' \\Zq\n" \
+	"_)      \\.___.|     .'\n" \
+	"\\____   )MMMMMM|   .'\n" \
+	"     `-'       `--'\n";
 
-// 	printf("%s\n", ascii);
-// }
+	printf("%s\n", ascii);
+}
 
-// /*ft_prompt recieves environment, finds PWD, HOME and USER,
-// finds if the current dir contains HOME dir and creates
-// a str that simulates bash prompt*/
+/*ft_prompt recieves environment, finds PWD, HOME and USER,
+finds if the current dir contains HOME dir and creates
+a str that simulates bash prompt*/
 
-// char	*ft_prompt(char **env)
-// {
-// 	char	*str;
-// 	char	*pwd;
-// 	char	*tmp;
-// 	char	*usr;
-// 	int		size;
+char	*ft_prompt(char **env)
+{
+	char	*str;
+	char	*pwd;
+	char	*tmp;
+	char	*usr;
+	int		size;
 
-// 	tmp = ft_strdup(ft_find_var(env, "PWD"));
-// 	size = ft_strlen(ft_find_var(env, "HOME"));
-// 	pwd = tmp;
-// 	if (ft_strnstr(pwd, ft_find_var(env, "HOME"), size))
-// 	{
-// 		pwd += size - 1;
-// 		pwd[0] = '~';
-// 	}
-// 	usr = ft_find_var(env, "USER");
-// 	size = ft_strlen(pwd) + ft_strlen(usr) + 14;
-// 	str = malloc((size + 1) * sizeof(char));
-// 	if (!str || !tmp)
-// 		return (NULL);
-// 	if (!ft_strlcpy(str, usr, size) || !ft_strlcat(str, "@minishell:", size) \
-// 		|| !ft_strlcat(str, pwd, size) || !ft_strlcat(str, "$ ", size))
-// 		return (NULL);
-// 	str[size] = '\0';
-// 	free(tmp);
-// 	return (str);
-// }
+	tmp = ft_strdup(ft_find_var(env, "PWD"));
+	size = ft_strlen(ft_find_var(env, "HOME"));
+	pwd = tmp;
+	if (ft_strnstr(pwd, ft_find_var(env, "HOME"), size))
+	{
+		pwd += size - 1;
+		pwd[0] = '~';
+	}
+	usr = ft_find_var(env, "USER");
+	size = ft_strlen(pwd) + ft_strlen(usr) + 14;
+	str = malloc((size + 1) * sizeof(char));
+	if (!str || !tmp)
+		return (NULL);
+	if (!ft_strlcpy(str, usr, size) || !ft_strlcat(str, "@minishell:", size) \
+		|| !ft_strlcat(str, pwd, size) || !ft_strlcat(str, "$ ", size))
+		return (NULL);
+	str[size] = '\0';
+	free(tmp);
+	return (str);
+}
 
-// int	main(int argc, char **argv, char **envp)
-// {
-// 	char	*input;
-// 	char	*str;
-// 	char	*prompt;
-// 	t_msh	msh;
+int	main(int argc, char **argv, char **envp)
+{
+	char	*input;
+	char	*str;
+	char	*prompt;
+	t_msh	msh;
+	t_cmd	*cmd;
+	t_list	*aux_lst;
+	int		n_cmd;
 
-// 	ft_draw();
-// 	msh.env = ft_env_parser(envp);
-// 	while (1)
-// 	{
-// 		prompt = ft_prompt(msh.env);
-// 		input = readline(prompt);
-// 		printf("input = %s\n", input);
-// 		str = ft_expand_vars(msh.env, input);
-// 		printf("expanded str = %s\n", str);
-// 		str = ft_expand_home(msh.env, str);
-// 		printf("expanded home = %s\n", str);
-// 		msh.lst = ft_proc_str(str, &msh);
-// 		ft_print_list(&msh);
-// 		free(str);
-// 		if (input == NULL)
-// 			break ;
-// 	}
-// 	ft_free_env(msh.env);
-// 	return (0);
-// }
+	ft_draw();
+	msh.env = ft_env_parser(envp);
+	get_path(&msh);
+	n_cmd = 1;
+	while (1)
+	{
+		prompt = ft_prompt(msh.env);
+		input = readline(prompt);
+		str = ft_expand_vars(msh.env, input);
+		str = ft_expand_home(msh.env, str);
+		msh.lst = ft_proc_str(str, &msh);
+		aux_lst = msh.lst;
+		while (aux_lst)
+		{
+			cmd = ((t_cmd *) aux_lst->content);
+			if (cmd->built)
+				ex_built();
+			else
+				ex_native(msh, cmd, n_cmd);
+			aux_lst = aux_lst->next;
+		}
+		free(str);
+		if (input == NULL)
+			break ;
+	}
+	ft_free_env(msh.env);
+	return (0);
+}
