35db4c1f0e87e0993a3a3f4784c2ef28eb957a0d
JordiDevv <jordidevv@gmail.com>
2025-03-24 13:52:25 +0100

    Se borra un print de debuggeo.

--- src/redirections/open_file.c
+++ src/redirections/open_file.c
@@ -6,7 +6,7 @@
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 12:50:50 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2025/03/24 13:41:10 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2025/03/24 13:52:01 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ void open_file(t_data *data, t_cmd *cmd)
 			data->fd_output = open(cmd->append, O_WRONLY | O_CREAT
 									| O_APPEND, S_IRUSR | S_IWUSR);
 	}
-	printf("%d, %s, %s\n", data->fd_output, cmd->output, cmd->append);
 	if ((data->fd_input && data->fd_input < 0)
 		|| (data->fd_output && data->fd_output < 0))
 	{
