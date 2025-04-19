5040a23aae3ee0ad5f37bcb1ac23c7cee85fb7d7
JordiDevv <jordidevv@gmail.com>
2025-04-20 00:52:36 +0200

    Se han marcado unas directrices sobre cómo debería ser el comportamiento del built-in para export.

---
+++ src/built-ins/export.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 00:45:33 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2025/04/20 00:51:36 by jsanz-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/executor.h"
+
+int ex_export(char **envp)
+{
+    /*Crear una función "ft_realloc" que reserve memoria según se pida, copie todo lo que quepa
+    en el nuevo string y libere el anterior? devolviendo al final el nuevo string*/
+
+    //Deberíamos utilizar esa función para modificar envp y añadir al final la nueva variable
+
+    /*En data deberíamos agregar una nueva matriz que almacene las variables exportadas en formato
+    "declare -x MYVAR=value". Solo pueden empezar por letras o guiones bajos*/
+
+    //Tenemos que aclarar con que debería empezar esa matriz
+}
