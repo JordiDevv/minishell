f1cd8d390d7d265358ca7a857d41841611a67e1a
JordiDevv <jordidevv@gmail.com>
2025-04-21 13:37:37 +0200

    Se modifica la cantidad para un write del built-in export.

--- src/built-ins/export.c
+++ src/built-ins/export.c
@@ -6,7 +6,7 @@
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 00:45:33 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2025/04/21 12:46:46 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2025/04/21 13:36:21 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int ex_export(t_msh *msh, t_data *data)
     i = 0;
     while (data->exported_vars[i])
     {
-        write(1, "declare -x ", 12);
+        write(1, "declare -x ", 11);
         write(1, data->exported_vars[i], ft_strlen(data->exported_vars[i]));
         write(1, "\n", 1);
         i++;
