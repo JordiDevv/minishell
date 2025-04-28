c415ecd7e975e39b018e7e79502dcc76206d52b8
Rodrigo Hernandez De Miguel <rhernand@c3r9s1.42malaga.com>
2025-04-28 12:20:06 +0200

    prompt.c

--- src/main_testing.c
+++ src/main_testing.c
@@ -6,7 +6,7 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 13:38:57 by rhernand          #+#    #+#             */
-/*   Updated: 2025/04/27 11:23:58 by rhernand         ###   ########.fr       */
+/*   Updated: 2025/04/28 12:14:13 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,39 +80,6 @@ void	ft_draw(void)
 	"     `-'       `--'\n";
 
 	printf("%s\n", ascii);
-}
-
-/*ft_prompt recieves environment, finds PWD, HOME and USER,
-finds if the current dir contains HOME dir and creates
-a str that simulates bash prompt*/
-
-char	*ft_prompt(char **env)
-{
-	char	*str;
-	char	*pwd;
-	char	*tmp;
-	char	*usr;
-	int		size;
-
-	tmp = ft_strdup(ft_find_var(env, "PWD"));
-	size = ft_strlen(ft_find_var(env, "HOME"));
-	pwd = tmp;
-	if (ft_strnstr(pwd, ft_find_var(env, "HOME"), size))
-	{
-		pwd += size - 1;
-		pwd[0] = '~';
-	}
-	usr = ft_find_var(env, "USER");
-	size = ft_strlen(pwd) + ft_strlen(usr) + 14;
-	str = malloc((size + 1) * sizeof(char));
-	if (!str || !tmp)
-		return (NULL);
-	if (!ft_strlcpy(str, usr, size) || !ft_strlcat(str, "@minishell:", size)
-		|| !ft_strlcat(str, pwd, size) || !ft_strlcat(str, "$ ", size))
-		return (NULL);
-	str[size] = '\0';
-	free(tmp);
-	return (str);
 }
 
 static void	init_minishell(t_data *data, t_msh *msh, char **envp)
