e3cd89dde4dd301c7efda6445541b55d77628a64
JordiDevv <jordidevv@gmail.com>
2025-05-06 14:20:25 +0200

    Refactorizado el flujo del here_doc.

--- src/redirections/redirect.c
+++ src/redirections/redirect.c
@@ -6,7 +6,7 @@
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 12:15:42 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2025/05/06 00:20:48 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2025/05/06 14:19:45 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,50 +35,59 @@ int	redirect(int input, int output, t_data *data)
 	return (0);
 }
 
-static void	input_heredoc(t_data *data, t_cmd *cmd)
+static void	heredoc_loop(t_data* data, t_cmd *cmd)
 {
 	char	*line;
-	int		status;
 
+	while (1)
+	{
+		write(1, "> ", 2);
+		line = ft_get_next_line(0);
+		if (!ft_strncmp(line, cmd->del, ft_strlen(cmd->del)))
+		{
+			if (line)
+				free(line);
+			break ;
+		}
+		write(data->pipe_fds[data->pipe_index][1], line, ft_strlen(line));
+		free(line);
+	}
+}
+
+static void	close_heredoc(t_data *data)
+{
+	int	status;
+
+	close(data->pipe_fds[data->pipe_index][1]);
+	data->fd_stdin = dup(STDIN_FILENO);
+	dup2(data->pipe_fds[data->pipe_index][0], STDIN_FILENO);
+	close(data->pipe_fds[data->pipe_index][0]);
+	waitpid(data->pids[data->pipe_index], &status, 0);
+	if (WIFSIGNALED(status))
+		g_exit_status = (128 + WTERMSIG(status));
+	else if ((WIFEXITED(status)))
+		g_exit_status = WEXITSTATUS(status);
+	data->hd_flag = 1;
+	data->pipe_index++;
+}
+
+static void	input_heredoc(t_data *data, t_cmd *cmd)
+{
 	data->pids[data->pipe_index] = fork();
 	if (data->pids[data->pipe_index] < 0)
 	{
-		write(2, "Error forking for executing a comand\n", 37);
+		write(2, "Error forking for executing a command\n", 38);
 		return ;
 	}
 	else if (data->pids[data->pipe_index] == 0)
 	{
 		close(data->pipe_fds[data->pipe_index][0]);
-		while (1)
-		{
-			write(1, "> ", 2);
-			line = ft_get_next_line(0);
-			if (!ft_strncmp(line, cmd->del, ft_strlen(cmd->del)))
-			{
-				if (line)
-					free(line);
-				break ;
-			}
-			write(data->pipe_fds[data->pipe_index][1], line, ft_strlen(line));
-			free(line);
-		}
+		heredoc_loop(data, cmd);
 		close(data->pipe_fds[data->pipe_index][1]);
 		exit(0);
 	}
 	else
-	{
-		close(data->pipe_fds[data->pipe_index][1]);
-		data->fd_stdin = dup(STDIN_FILENO);
-		dup2(data->pipe_fds[data->pipe_index][0], STDIN_FILENO);
-		close(data->pipe_fds[data->pipe_index][0]);
-		waitpid(data->pids[data->pipe_index], &status, 0);
-		if (WIFSIGNALED(status))
-			g_exit_status = (128 + WTERMSIG(status));
-		else if ((WIFEXITED (status)))
-			g_exit_status = WEXITSTATUS(status);
-		data->hd_flag = 1;
-		data->pipe_index++;
-	}
+		close_heredoc(data);
 }
 
 void	file_redirection(t_data *data, t_cmd *cmd)
