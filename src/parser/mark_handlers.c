46bd6bb2f0da9c14324037cac295a274a5d48bd0
rhernand <rhernand@student.42malaga.com>
2025-05-11 13:00:26 +0200

    cannot make it work

--- src/parser/mark_handlers.c
+++ src/parser/mark_handlers.c
@@ -6,7 +6,7 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 12:54:44 by rhernand          #+#    #+#             */
-/*   Updated: 2025/05/10 13:02:05 by rhernand         ###   ########.fr       */
+/*   Updated: 2025/05/11 12:59:09 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,6 @@ void	ft_sub_line(t_msh *msh, int *pipe_fd)
 			exit (EXIT_FAILURE);
 		}
 		aux = ft_strjoin(msh->input, next_line);
-		free (msh->input);
 		free (next_line);
 		msh->input = aux;
 	}
@@ -101,28 +100,38 @@ void	ft_prompt_marks(t_msh *msh)
 		return ;
 	tmp = NULL;
 	pipe(pipe_fd);
-	signal(SIGINT, SIG_IGN);
-	pid = fork();
-	if (pid == 0)
-		ft_sub_line(msh, pipe_fd);
-	else
+	if (ft_count_marks(msh->input))
 	{
-		signal(SIGINT, sigint_handler);
-		waitpid(pid, &status, 0);
-		close(pipe_fd[1]);
-		while (1)
+		signal(SIGINT, SIG_IGN);
+		pid = fork();
+		if (pid == 0)
+			ft_sub_line(msh, pipe_fd);
+		else
 		{
-			aux = ft_get_next_line(pipe_fd[0]);
-			if (!aux)
-				break ;
-			tmp = ft_strjoin(msh->input, aux);
-			free (aux);
-			free (msh->input);
-			msh->input = tmp;
+			waitpid(pid, &status, 0);
+			if (WIFSIGNALED (status) && WTERMSIG(status) == SIGINT)
+			{
+				write(STDIN_FILENO, "\n", 1);
+				free (msh->input);
+				msh->input = NULL;
+				close (pipe_fd[0]);
+				signal(SIGINT, sigint_handler);
+				return ;
+			}
+			signal(SIGINT, sigint_handler);
+			close(pipe_fd[1]);
+			while (1)
+			{
+				aux = ft_get_next_line(pipe_fd[0]);
+				if (!aux)
+					break ;
+				tmp = ft_strjoin(msh->input, aux);
+				free (aux);
+				free (msh->input);
+				msh->input = tmp;
+			}
+			close(pipe_fd[0]);
 		}
-		close(pipe_fd[0]);
-		if (WIFSIGNALED (status))
-			msh->input = NULL;
 	}
 }
 
